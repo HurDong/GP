d56ca9e103b9d23e5694fcb5ae59038d8d669233
Hur <gjehdals456@gmail.com>
2023-04-26 23:09:10 +0900

    third commit

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ searchBox.addEventListener("keydown", function (event) {
 });
 
 function searchAddress(keyword) {
-  geocoder.addressSearch(keyword, function (result, status) {
+  var ps = new kakao.maps.services.Places(map);
+  ps.keywordSearch(keyword, function (result, status, pagination) {
     if (status === kakao.maps.services.Status.OK) {
       showAddressList(result);
     } else {
@@ -63,7 +64,7 @@ function showAddressList(addresses) {
 
   addresses.forEach(function (address) {
     var li = document.createElement("li");
-    li.innerText = address.address_name;
+    li.innerText = address.place_name + " - " + address.address_name;
     li.onclick = function () {
       addAddressMarker(address);
       searchResultWindow.close();
@@ -80,9 +81,10 @@ function addAddressMarker(address) {
 
   var addressList = document.getElementById("address-list");
   var li = document.createElement("li");
-  li.innerText = address.address_name;
+  li.innerText = address.place_name + " - " + address.address_name;
   addressList.appendChild(li);
 }
+
 // 중간 지점 계산 및 표시
 document.getElementById("meet-button").addEventListener("click", function () {
   if (markers.length >= 2) {
@@ -98,8 +100,30 @@ document.getElementById("meet-button").addEventListener("click", function () {
       ),
     });
     centerMarker.setMap(map);
+    document.getElementById("time-button").disabled = false;
   } else {
     alert("적어도 2개의 마커가 필요합니다.");
   }
 });
-// hello
+
+// 시간 계산 창 호출
+document.getElementById("time-button").addEventListener("click", function () {
+  const travelTimeWindow = window.open(
+    "",
+    "travelTimeWindow",
+    "width=400,height=600"
+  );
+  travelTimeWindow.document.write(
+    "<h2>각 주소에서 중간 지점까지의 이동 시간</h2>"
+  );
+
+  const list = document.createElement("ul");
+  travelTimeWindow.document.body.appendChild(list);
+
+  // 각 주소에 대한 시간 계산 (예시로 표시)
+  for (let i = 0; i < markers.length; i++) {
+    const li = document.createElement("li");
+    li.innerText = `${i + 1}번: ${Math.floor(Math.random() * 60) + 20}분`;
+    list.appendChild(li);
+  }
+});
