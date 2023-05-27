f47628a9a391f6b39b0520f57dd064ed03527fe8
HurDong <gjehdals456@gmail.com>
2023-05-28 05:18:31 +0900

    commit 0528 0518

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+const openai = require("openai");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+openai.apiKey = "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"; // 실제 키로 바꾸세요.
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/generate", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  try {
+    const gptResponse = await openai.Completion.create({
+      engine: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 100,
+    });
+
+    const message = gptResponse.choices[0].text.strip();
+    res.json({ message: message });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while generating a message." });
+  }
 });
 
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
