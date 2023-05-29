545855b7eabb50943ddf3860f6ae578bc4acd3a2
HurDong <gjehdals456@gmail.com>
2023-05-29 21:15:41 +0900

    commit 0529 2115

---
+++ meedingDB_01.sql
@@ -0,0 +1,28 @@
+USE meetingDB;
+
+DROP TABLE IF EXISTS Users, Restaurants, Reviews;
+
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE Restaurants (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    description TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE Reviews (
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    restaurant_id INT,
+    rating INT NOT NULL,
+    review_text TEXT,
+    PRIMARY KEY (id), 
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (restaurant_id) REFERENCES Restaurants(id)
+);
--- server.js
+++ server.js
@@ -1,32 +1,173 @@
-const express = require("express");
-const openai = require("openai");
-const dotenv = require("dotenv");
+var express = require("express");
+var mysql = require("mysql");
+var app = express();
 
-dotenv.config();
+// Allow express to parse JSON bodies in POST requests
+app.use(express.json());
 
-openai.apiKey = process.env.OPENAI_API_KEY;
-
-const app = express();
-
-app.get("/generate-text", async (req, res) => {
-  const prompt = req.query.prompt;
-  try {
-    const gptResponse = await openai.Completion.create({
-      engine: "text-davinci-002",
-      prompt: prompt,
-      max_tokens: 100,
-    });
-
-    return res.json({
-      generated_text: gptResponse.choices[0].text,
-    });
-  } catch (err) {
-    return res.json({
-      error: "An error occurred while generating a message.",
-    });
-  }
+// Create a connection to the database
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "meetingDB",
 });
 
-const port = process.env.PORT || 3000;
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to the database!");
+});
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.get("/reviews", function (req, res) {
+  connection.query("SELECT * FROM reviews", (err, mysqlres) => {
+    if (err) {
+      console.log("Error fetching reviews: " + err);
+      res.json({ status: "error" });
+    } else {
+      res.json({ status: "ok", data: mysqlres });
+    }
+  });
+});
+
+app.get("/reviews/:id", function (req, res) {
+  connection.query(
+    "SELECT * FROM reviews WHERE id = ?",
+    [req.params.id],
+    (err, mysqlres) => {
+      if (err) {
+        console.log("Error fetching review with id: " + req.params.id);
+        res.json({ status: "error" });
+      } else {
+        res.json({ status: "ok", data: mysqlres });
+      }
+    }
+  );
+});
+
+app.put("/reviews/:id", function (req, res) {
+  const { rating, review_text } = req.body;
+  connection.query(
+    "UPDATE reviews SET rating = ?, review_text = ? WHERE id = ?",
+    [rating, review_text, req.params.id],
+    (err) => {
+      if (err) {
+        console.log("Error updating review: " + err);
+        res.json({ status: "error" });
+      } else {
+        res.json({ status: "ok" });
+      }
+    }
+  );
+});
+
+app.delete("/reviews/:id", function (req, res) {
+  connection.query(
+    "DELETE FROM reviews WHERE id = ?",
+    [req.params.id],
+    (err) => {
+      if (err) {
+        console.log("Error deleting review: " + err);
+        res.json({ status: "error" });
+      } else {
+        res.json({ status: "ok" });
+      }
+    }
+  );
+});
+
+app.get("/reviews/restaurant/:restaurant_id", (req, res) => {
+  connection.query(
+    "SELECT * FROM reviews WHERE restaurant_id = ?",
+    [req.params.restaurant_id],
+    (err, results) => {
+      if (err) throw err;
+      res.send(results);
+    }
+  );
+});
+
+app.get("/users/:id", function (req, res) {
+  connection.query(
+    "SELECT * FROM Users WHERE id = ?",
+    [req.params.id],
+    (err, mysqlres) => {
+      if (err) {
+        console.log("Error fetching user with id: " + req.params.id);
+        res.json({ status: "error" });
+      } else {
+        res.json({ status: "ok", data: mysqlres });
+      }
+    }
+  );
+});
+
+app.post("/signup", function (req, res) {
+  const { username, email, password } = req.body;
+
+  // Check if any of the fields are empty
+  if (!username || !email || !password) {
+    res.json({ status: "error", message: "Please fill all fields" });
+    return;
+  }
+
+  // Check if the email is already registered
+  connection.query(
+    "SELECT * FROM Users WHERE email = ?",
+    [email],
+    function (err, result) {
+      if (err) {
+        res.json({
+          status: "error",
+          message: "An error occurred during sign up1",
+        });
+        console.log("Error fetching users: " + err);
+        return;
+      }
+
+      if (result.length > 0) {
+        res.json({ status: "error", message: "Email already registered" });
+        return;
+      } else {
+        // Insert the new user into the database
+        connection.query(
+          "INSERT INTO Users (username, email, password) VALUES (?, ?, ?)",
+          [username, email, password],
+          function (err, result) {
+            if (err) {
+              res.json({
+                status: "error",
+                message: "An error occurred during sign up2",
+              });
+              console.log("Error inserting new user: " + err);
+              return;
+            }
+
+            res.json({ status: "ok", message: "Successfully registered" });
+          }
+        );
+      }
+    }
+  );
+});
+
+app.post("/login", function (req, res) {
+  const { email, password } = req.body;
+  connection.query(
+    "SELECT * FROM Users WHERE email = ? AND password = ?",
+    [email, password],
+    function (err, results) {
+      if (err) {
+        console.log("Error logging in: " + err);
+        res.json({ status: "error" });
+      } else if (results.length > 0) {
+        res.json({ status: "ok", data: results[0] });
+      } else {
+        res.json({ status: "error", message: "Invalid email/password" });
+      }
+    }
+  );
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000");
+});
